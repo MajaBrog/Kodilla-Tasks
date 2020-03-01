34d20d9c3edcf7f4d83e3f45289d7b4c5224ee56
MajaBrog <majaa.brog@gmail.com>
2020-03-01 21:05:54 +0100

    23.1

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,4 +1,4 @@
-package com.crud.tasks.application;
+package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
